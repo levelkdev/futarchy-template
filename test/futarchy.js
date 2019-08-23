2544df7a0e21b044b3b3b2207bfe370cb8f8b4b6
Mike Calvanese <mfcalvanese@gmail.com>
2019-08-23 14:46:03 -0700

    add tests for voting and token-manager apps

--- test/futarchy.js
+++ test/futarchy.js
@@ -3,6 +3,7 @@ const { randomId } = require('@aragon/templates-shared/helpers/aragonId')
 const { getEventArgument } = require('@aragon/test-helpers/events')
 const { deployedAddresses } = require('@aragon/templates-shared/lib/arapp-file')(web3)
 const { getInstalledAppsById } = require('@aragon/templates-shared/helpers/events')(artifacts)
+const { assertRole, assertMissingRole } = require('@aragon/templates-shared/helpers/assertRole')(web3)
 
 const FutarchyTemplate = artifacts.require('FutarchyTemplate')
 
@@ -10,6 +11,7 @@ const ENS = artifacts.require('ENS')
 const ACL = artifacts.require('ACL')
 const Kernel = artifacts.require('Kernel')
 const Voting = artifacts.require('Voting')
+const TokenManager = artifacts.require('TokenManager')
 const MiniMeToken = artifacts.require('MiniMeToken')
 const PublicResolver = artifacts.require('PublicResolver')
 
@@ -18,7 +20,7 @@ const ONE_WEEK = ONE_DAY * 7
 
 contract('FutarchyTemplate', ([_, owner, holder1, holder2]) => {
   let daoID, template, dao, ens
-  let token
+  let voting, tokenManager, token
 
   const HOLDERS = [holder1, holder2]
   const STAKES = HOLDERS.map(() => 1e18)
@@ -43,9 +45,12 @@ contract('FutarchyTemplate', ([_, owner, holder1, holder2]) => {
     const installedApps = getInstalledAppsById(instanceReceipt)
 
     assert.equal(dao.address, getEventArgument(instanceReceipt, 'SetupDao', 'dao'), 'should have emitted a SetupDao event')
-
+    
     assert.equal(installedApps.voting.length, 1, 'should have installed 1 voting app')
     voting = Voting.at(installedApps.voting[0])
+
+    assert.equal(installedApps['token-manager'].length, 1, 'should have installed 1 token manager app')
+    tokenManager = TokenManager.at(installedApps['token-manager'][0])
   }
 
   const testDAOSetup = () => {
@@ -65,6 +70,29 @@ contract('FutarchyTemplate', ([_, owner, holder1, holder2]) => {
     it('mints requested amounts for the holders', async () => {
       assert.equal((await token.totalSupply()).toString(), STAKES.reduce((a, b) => a + b))
       for (const holder of HOLDERS) assert.equal((await token.balanceOf(holder)).toString(), STAKES[HOLDERS.indexOf(holder)])
+    })
+
+    it('should have voting app correctly setup', async () => {
+      assert.isTrue(await voting.hasInitialized(), 'voting not initialized')
+      assert.equal((await voting.supportRequiredPct()).toString(), SUPPORT_REQUIRED)
+      assert.equal((await voting.minAcceptQuorumPct()).toString(), MIN_ACCEPTANCE_QUORUM)
+      assert.equal((await voting.voteTime()).toString(), VOTE_DURATION)
+
+      await assertRole(acl, voting, voting, 'CREATE_VOTES_ROLE', tokenManager)
+      await assertRole(acl, voting, voting, 'MODIFY_QUORUM_ROLE')
+      await assertRole(acl, voting, voting, 'MODIFY_SUPPORT_ROLE')
+    })
+
+    it('should have token manager app correctly setup', async () => {
+      assert.isTrue(await tokenManager.hasInitialized(), 'token manager not initialized')
+      assert.equal(await tokenManager.token(), token.address)
+
+      await assertRole(acl, tokenManager, voting, 'MINT_ROLE')
+      await assertRole(acl, tokenManager, voting, 'BURN_ROLE')
+
+      await assertMissingRole(acl, tokenManager, 'ISSUE_ROLE')
+      await assertMissingRole(acl, tokenManager, 'ASSIGN_ROLE')
+      await assertMissingRole(acl, tokenManager, 'REVOKE_VESTINGS_ROLE')
     })
   }
 
