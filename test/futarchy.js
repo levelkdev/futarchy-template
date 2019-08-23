350a74db1edb6fa5bdda8963f948625faee2a9ff
Mike Calvanese <mfcalvanese@gmail.com>
2019-08-23 13:39:03 -0700

    initial code for FutarchyTemplate.sol contract and tests
    mostly copied from https://github.com/aragon/dao-templates/blob/master/templates/company/contracts/CompanyTemplate.sol with finance, agent, and vault apps removed

---
+++ test/futarchy.js
@@ -0,0 +1,83 @@
+const { hash: namehash } = require('eth-ens-namehash')
+const { randomId } = require('@aragon/templates-shared/helpers/aragonId')
+const { getEventArgument } = require('@aragon/test-helpers/events')
+const { deployedAddresses } = require('@aragon/templates-shared/lib/arapp-file')(web3)
+const { getInstalledAppsById } = require('@aragon/templates-shared/helpers/events')(artifacts)
+
+const FutarchyTemplate = artifacts.require('FutarchyTemplate')
+
+const ENS = artifacts.require('ENS')
+const ACL = artifacts.require('ACL')
+const Kernel = artifacts.require('Kernel')
+const Voting = artifacts.require('Voting')
+const MiniMeToken = artifacts.require('MiniMeToken')
+const PublicResolver = artifacts.require('PublicResolver')
+
+const ONE_DAY = 60 * 60 * 24
+const ONE_WEEK = ONE_DAY * 7
+
+contract('FutarchyTemplate', ([_, owner, holder1, holder2]) => {
+  let daoID, template, dao, ens
+  let token
+
+  const HOLDERS = [holder1, holder2]
+  const STAKES = HOLDERS.map(() => 1e18)
+  const TOKEN_NAME = 'Share Token'
+  const TOKEN_SYMBOL = 'SHARE'
+
+  const VOTE_DURATION = ONE_WEEK
+  const SUPPORT_REQUIRED = 50e16
+  const MIN_ACCEPTANCE_QUORUM = 5e16
+  const VOTING_SETTINGS = [SUPPORT_REQUIRED, MIN_ACCEPTANCE_QUORUM, VOTE_DURATION]
+
+  before('fetch futarchy template and ENS', async () => {
+    const { registry, address } = await deployedAddresses()
+    ens = ENS.at(registry)
+    template = FutarchyTemplate.at(address)
+  })
+
+  const loadDAO = async (tokenReceipt, instanceReceipt) => {
+    dao = Kernel.at(getEventArgument(instanceReceipt, 'DeployDao', 'dao'))
+    token = MiniMeToken.at(getEventArgument(tokenReceipt, 'DeployToken', 'token'))
+    acl = ACL.at(await dao.acl())
+    const installedApps = getInstalledAppsById(instanceReceipt)
+
+    assert.equal(dao.address, getEventArgument(instanceReceipt, 'SetupDao', 'dao'), 'should have emitted a SetupDao event')
+
+    assert.equal(installedApps.voting.length, 1, 'should have installed 1 voting app')
+    voting = Voting.at(installedApps.voting[0])
+  }
+
+  const testDAOSetup = () => {
+    it('registers a new DAO on ENS', async () => {
+      const aragonIdNameHash = namehash(`${daoID}.aragonid.eth`)
+      const resolvedAddress = await PublicResolver.at(await ens.resolver(aragonIdNameHash)).addr(aragonIdNameHash)
+      assert.equal(resolvedAddress, dao.address, 'aragonId ENS name does not match')
+    })
+
+    it('creates a new token', async () => {
+      assert.equal(await token.name(), TOKEN_NAME)
+      assert.equal(await token.symbol(), TOKEN_SYMBOL)
+      assert.equal(await token.transfersEnabled(), true)
+      assert.equal((await token.decimals()).toString(), 18)
+    })
+
+    it('mints requested amounts for the holders', async () => {
+      assert.equal((await token.totalSupply()).toString(), STAKES.reduce((a, b) => a + b))
+      for (const holder of HOLDERS) assert.equal((await token.balanceOf(holder)).toString(), STAKES[HOLDERS.indexOf(holder)])
+    })
+  }
+
+  const createDAO = () => {
+    before('create company entity', async () => {
+      daoID = randomId()
+      receipt = await template.newTokenAndInstance(TOKEN_NAME, TOKEN_SYMBOL, daoID, HOLDERS, STAKES, VOTING_SETTINGS, { from: owner })
+      await loadDAO(receipt, receipt)
+    })
+  }
+
+  context('creating instances with a single transaction', () => {
+    createDAO()
+    testDAOSetup()
+  })
+})
