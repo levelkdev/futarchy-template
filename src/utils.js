160e5846c543dabc24c49432be8d3207c74b444e
Mike Calvanese <mfcalvanese@gmail.com>
2019-11-21 14:06:19 -0700

    convert numbers from scientific notation to full number string

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,16 @@
 const web3Utils = require('web3-utils')
 
 function numberToBytes32 (number) {
-  const hex = web3Utils.toHex(number)
+  const hex = web3Utils.toHex(toNumberString(number))
   return web3Utils.padLeft(hex, 64)
 }
 
 function addressToBytes32 (address) {
   return web3Utils.padLeft(address, 64)
+}
+
+function toNumberString(n) {
+  return n.toLocaleString('fullwide', {useGrouping:false})
 }
 
 module.exports = {
